f62524e49466aa9e24ce874a90068937755a3921
MichalDabert <michal.dabert@gmail.com>
2023-02-01 14:21:47 +0100

    esc clear and 1-8

--- bitstohex.py
+++ bitstohex.py
@@ -4,7 +4,7 @@ from PySide6.QtWidgets import QApplication, QCheckBox, QHBoxLayout, QLabel, QVBo
 
 
 def bits_to_hex(b1, b2, b3, b4, b5, b6, b7, b8):
-    bits = [1 if b else 0 for b in [b1, b2, b3, b4, b5, b6, b7, b8]] # list comprehension
+    bits = [1 if b else 0 for b in [b1, b2, b3, b4, b5, b6, b7, b8]]  # list comprehension
     decimal = int(''.join(map(str, bits)), 2)
     return hex(decimal)
 
@@ -37,6 +37,28 @@ class CheckboxWidget(QWidget):
         b1, b2, b3, b4, b5, b6, b7, b8 = [checkbox.isChecked() for checkbox in self.checkboxes]
         hex_value = bits_to_hex(b1, b2, b3, b4, b5, b6, b7, b8)
         self.label.setText(hex_value)
+
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key_1:
+            self.checkboxes[0].setChecked(not self.checkboxes[0].isChecked())
+        elif event.key() == Qt.Key_2:
+            self.checkboxes[1].setChecked(not self.checkboxes[1].isChecked())
+        elif event.key() == Qt.Key_3:
+            self.checkboxes[2].setChecked(not self.checkboxes[2].isChecked())
+        elif event.key() == Qt.Key_4:
+            self.checkboxes[3].setChecked(not self.checkboxes[3].isChecked())
+        elif event.key() == Qt.Key_5:
+            self.checkboxes[4].setChecked(not self.checkboxes[4].isChecked())
+        elif event.key() == Qt.Key_6:
+            self.checkboxes[5].setChecked(not self.checkboxes[5].isChecked())
+        elif event.key() == Qt.Key_7:
+            self.checkboxes[6].setChecked(not self.checkboxes[6].isChecked())
+        elif event.key() == Qt.Key_8:
+            self.checkboxes[7].setChecked(not self.checkboxes[7].isChecked())
+        elif event.key() == Qt.Key_Escape:
+            for checkbox in self.checkboxes:
+                checkbox.setChecked(False)
+        self.update_label()
 
 
 if __name__ == "__main__":
